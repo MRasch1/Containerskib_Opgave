7165e426c06503eb56b005e5f9068c98bfba691e
Magnus Aleksander Bilsted Rasch <mrasch1102@gmail.com>
2023-08-07 14:05:42 +0200

    Add project files.

---
+++ Containerskib_Opgave/ClassA.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Containerskib_Opgave
+{
+    internal class ClassA
+    {
+        ClassB classB = new ClassB();
+
+
+
+        public ClassA()
+        {
+            Console.Write("Indtast et input i tal for at få et resultat: ");
+            classB.input = Convert.ToDouble(Console.ReadLine());
+            classB.KnobTilKmt(classB.input);
+            classB.KmtTilKnob(classB.input);
+            Console.WriteLine("Dette er resultatet beregnet fra knob til km/t: " + Math.Round(classB.km, 2));
+            Console.WriteLine("Dette er resultatet beregnet fra km/t til knob: " + Math.Round(classB.knob, 2));
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ Containerskib_Opgave/ClassB.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Containerskib_Opgave
+{
+    internal class ClassB
+    {
+        public double input;
+
+        public ClassB()
+        {
+
+        }
+
+        private double Knob { get; set; }
+
+        public double KnobTilKmt(double knob)
+        {
+            input = knob;
+            return knob * 1.85;
+        }
+
+        public double KmtTilKnob(double km)
+        {
+            input = km;
+            return km / 1.85;
+        }
+    }
+}
